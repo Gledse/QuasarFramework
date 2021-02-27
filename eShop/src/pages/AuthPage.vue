38dbc55bea873eabf03f8281d6ea1234bc0f3d6e
Gledse_Jamisse <53790917+Gledse@users.noreply.github.com>
2021-02-27 17:29:41 +0200

    ..

--- eShop/src/pages/AuthPage.vue
+++ eShop/src/pages/AuthPage.vue
@@ -1,5 +1,10 @@
 <template>
+  <div class="q-pa-md text-center">
 
+    <h1>
+      Auth Page
+    </h1>
+  </div>
 </template>
 
 <script>
---
+++ eShop/src/pages/ProductDetailsPage.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "ProductDetailsPage"
+}
+</script>
+
+<style scoped>
+
+</style>
