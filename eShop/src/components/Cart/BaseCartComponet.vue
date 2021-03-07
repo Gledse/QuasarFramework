dcbce4c5820a3cb14ba1a1dea7ed24ec2d9638e2
Gledse_Jamisse <53790917+Gledse@users.noreply.github.com>
2021-03-08 00:04:43 +0200

    Update BaseCartComponet.vue

--- eShop/src/components/Cart/BaseCartComponet.vue
+++ eShop/src/components/Cart/BaseCartComponet.vue
@@ -12,17 +12,37 @@
 
   </templete>
 
-  <q-footer>
-    <div>
+  <q-footer class="bg-grey-2">
+    <div class="q-pa-md">
 
-      <div>
+      <div class="q-pa-md text-grey-8">
+        <div class="text-bold text-primary">Resumo da compra</div>
+
+        <div class="row q-pt-md">
+          <div class="col-6 text-bold">
+            Quantidade:
+          </div>
+          <div class="col-6  text-h6 text-right">
+            10 Produtos
+          </div>
+        </div>
+        <div class="row q-pb-md">
+          <div class="col-6 text-bold">
+            Valor Total:
+          </div>
+          <div class="col-6  text-h6 text-right">
+            89,00 MT
+          </div>
+        </div>
 
       </div>
       <div>
         <q-btn
           label="Pagar (MPesa)"
-          class="full-width"
+          dense
           color="primary"
+          class="full-width"
+          unelevated
         />
       </div>
 
