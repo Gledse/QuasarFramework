6d30601c14688678de69ad87bec16ee4ced58e1b
Gledse_Jamisse <53790917+Gledse@users.noreply.github.com>
2021-03-08 15:57:32 +0200

    Update products.js

--- eShop/src/store/modules/products.js
+++ eShop/src/store/modules/products.js
@@ -1,4 +1,16 @@
+
+import axios from 'axios'
+
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+
+
 const stateA = {
+
+  products: {},
+  SERVER_URL: 'https://lives.explicador.co.mz/api/',
+  AUTHORIZATION_KEY: '',
+  loading: false
 
 }
 const mutations = {
