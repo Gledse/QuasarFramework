7fc259654f69d3561f44c3d63921c66816e19f1b
Gledse_Jamisse <53790917+Gledse@users.noreply.github.com>
2021-03-08 14:08:18 +0200

    Update HeaderComponent.vue

--- eShop/src/components/base/HeaderComponent.vue
+++ eShop/src/components/base/HeaderComponent.vue
@@ -2,13 +2,23 @@
   <q-header elevated>
     <q-toolbar>
       <q-btn
+        v-if="isHomePage"
         @click="$emit('toggleDrawer')"
         flat
         round
         dense
         icon="menu"
-        class="q-mr-sm" />
-
+        class="q-mr-sm"
+      />
+      <q-btn
+        v-else
+        to="/"
+        flat
+        round
+        dense
+        icon="arrow_back"
+        class="q-mr-sm"
+      />
 
       <q-toolbar-title>
           {{ appTitle }}
@@ -16,7 +26,8 @@
 
       <div class = "q-gutter-sm q-mr-md">
         <q-btn flat round dense icon="add" to="/products/add"/>
-        <q-btn flat round dense icon="shopping_cart" to="/cart" />
+        <q-btn flat round dense icon="shopping_cart" to="/cart"/>
+        <q-badge color="red" floating>4</q-badge>
       </div>
 
       <q-btn round flat to="/profile">
@@ -41,7 +52,11 @@ export default {
     ]),
     ...mapState('auth', [
       'userAuthImage'
-    ])
+    ]),
+    isHomePage () {
+      let routPath = this.$route.fullPath
+      return routPath === '/'
+    }
   }
 }
 </script>
